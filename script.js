5e461418df3d36bc591c99f78b167ef5afebecc7
Arnavmishra142 <mishrarnav142@gmail.com>
2026-03-01 09:15:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==================== DOM ELEMENTS ====================
+const urlInput = document.getElementById('videoUrl');
+const fetchBtn = document.getElementById('fetchBtn');
+const btnText = document.getElementById('btnText');
+const loader = document.getElementById('loader');
+const statusMsg = document.getElementById('statusMsg');
+const resultCard = document.getElementById('resultCard');
+
+const thumbImg = document.getElementById('thumbImg');
+const vidTitle = document.getElementById('vidTitle');
+const dlLink = document.getElementById('dlLink');
+const platformBadge = document.getElementById('platformBadge');
+
+const navAbout = document.getElementById('navAbout');
+const aboutOverlay = document.getElementById('aboutOverlay');
+const closeAbout = document.getElementById('closeAbout');
